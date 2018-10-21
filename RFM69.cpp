dcb23b50097063347abebe34d83e90d22c09ccdb
Ivan Králik <ivan.kralik.sk@gmail.com>
2018-10-21 10:53:01 +0200

    - initial commit

---
+++ RFM69.cpp
@@ -0,0 +1,526 @@
+#include "RFM69.h"
+#include "RFM69_registers.h"
+#include <SPI.h>
+#include <DebugUtils.h>
+
+RFM69::RFM69(uint8_t dio0_pin, uint8_t ss_pin, uint8_t rst_pin):m_dio0_pin(dio0_pin),m_ss_pin(ss_pin),m_rst_pin(rst_pin)
+{
+  ;
+}
+
+void RFM69::begin()
+{
+  pinMode(m_dio0_pin, INPUT);
+  pinMode(m_ss_pin, OUTPUT);
+  pinMode(m_rst_pin, OUTPUT);
+  digitalWrite(m_ss_pin, HIGH);
+  digitalWrite(m_rst_pin, LOW);
+
+  SPI.begin();
+
+  DEBUGPRINTLN1("Resetting RFM69 module");
+  
+  reset();
+
+  if (getMode() != RFM69_MODE_STBY) {
+    DEBUGPRINTLN1("RFM69 module not present or wired wrong");
+
+    return;
+  } else {
+    m_ready = true;
+  }
+  
+  setChannel(m_channel);
+  
+  DEBUGPRINTLN1("Begin RFM69 OSC calibration");
+  calibrateOsc();
+  DEBUGPRINTLN1("Done RFM69 OSC calibration");
+
+  //writeReg(RFM69_REG_LISTEN_1, 0x92);
+  writeReg(RFM69_REG_LISTEN_1, 0x90);
+  writeReg(RFM69_REG_LISTEN_2, 0xF5);
+  writeReg(RFM69_REG_LISTEN_3, 0x28);
+  writeReg(RFM69_REG_RX_TIMEOUT_2, 0x40);
+  writeReg(RFM69_REG_PACKET_CONFIG_1, 0x94);
+  writeReg(RFM69_REG_PACKET_CONFIG_2, 0x12);
+  writeReg(RFM69_REG_FIFO_THRESH, 0x8F);
+  writeReg(RFM69_REG_PAYLOAD_LENGTH, RFM69_MAX_PACKET_LENGTH);
+  writeReg(RFM69_REG_RSSI_THRESH, ((uint8_t) -m_rssi_threshold) * 2);
+  writeReg(RFM69_REG_BROADCAST_ADRS, getBroadcastId());
+  writeReg(RFM69_REG_SYNC_CONFIG, 0x80);
+  writeReg(RFM69_REG_SYNC_VALUE_1, m_network_id);
+  writeReg(RFM69_REG_TEST_DAGC, 0x30);
+
+  randomSeed(analogRead(0));
+  m_seq_num = random(1, 256);
+
+  Serial.println(m_seq_num);
+}
+
+void RFM69::updateRssi()
+{
+  m_rssi = readReg(RFM69_REG_RSSI_VALUE);
+}
+
+void RFM69::update()
+{
+  if (!m_ready) {
+    return;
+  }
+
+  if (m_state == SEND_LBT && isModeReady()) {
+    if (isChannelFree()) {
+      beginTransmission();
+      DEBUGPRINTLN1("Transmitting message");
+      transmitMessage();
+      m_state = SEND_WAIT;
+    }
+  } else if (m_state == SEND_WAIT && isTransmissionFinished()) {
+    endTransmission();
+
+    if (m_recipient == getBroadcastId()) {
+      restoreDefaultMode();
+    } else {
+      setMode(RFM69_MODE_RX);
+      DEBUGPRINTLN1("Awaiting ack");
+      m_ack_wait_start = millis();
+      m_state = SEND_WAIT_ACK;
+    }
+  } else if (m_state == SEND_WAIT_ACK && isPayloadReady()) {
+    uint8_t ackLength;
+    int8_t rssi;
+    receivePacket(m_ack_buffer, &ackLength, sizeof(m_ack_buffer), &rssi);
+
+    if (m_ack_buffer[4] == m_send_seq_num) {
+      DEBUGPRINTLN1("Ack received");
+      restoreDefaultMode();
+    }
+  } else if (m_state == SEND_WAIT_ACK && millis() - m_ack_wait_start > 5) {
+    if (m_send_retries > 0) {
+      updateRssi();
+
+      if (getRssi() <= m_rssi_threshold) {
+        beginTransmission();
+        DEBUGPRINTLN1("Re-transmitting message");
+        transmitMessage();
+        m_send_retries--;
+        m_state = SEND_WAIT;
+      }
+    } else {
+      DEBUGPRINTLN1("Ack not received");
+      restoreDefaultMode();
+    }
+  } else if (m_state == ACK_WAIT && isTransmissionFinished()) {
+    endTransmission();
+
+    if (!isInHistory(m_ack_recipient, m_ack_seq_num)) {
+      putInHistory(m_ack_recipient, m_ack_seq_num);
+      doReceiveCallback();
+    }
+
+    restoreDefaultMode();
+  } else if (m_state == RECV && isPayloadReady()) {
+    receivePacket(m_packet_buffer, &m_packet_length, RFM69_MAX_PACKET_LENGTH, &m_recv_rssi);
+    uint8_t recipient = m_packet_buffer[2];
+    
+    if (recipient != getBroadcastId()) {
+      m_ack_recipient = recipient;
+      m_ack_seq_num = m_packet_buffer[3];
+      
+      beginTransmission();
+      DEBUGPRINTLN1("Packet received, transmitting ack");
+      transmitAck();
+      m_state = ACK_WAIT;
+    } else {
+      DEBUGPRINTLN1("Packet received");
+      doReceiveCallback();
+      restoreDefaultMode();
+    }
+  }
+}
+
+bool RFM69::isSending() {
+  return m_state == SEND_LBT || m_state == SEND_WAIT || m_state == SEND_WAIT_ACK;
+}
+
+void RFM69::waitForSend() {
+  while (isSending()) {
+    update();
+    yield();
+  }
+}
+
+void RFM69::setNetworkId(uint8_t network_id)
+{
+  m_network_id = network_id;
+  writeReg(RFM69_REG_SYNC_VALUE_1, m_network_id);
+}
+
+uint8_t RFM69::getNetworkId()
+{
+  return m_network_id;
+}
+
+void RFM69::setNodeId(uint8_t node_id)
+{
+  m_node_id = node_id;
+  writeReg(RFM69_REG_NODE_ADRS, node_id);
+}
+
+uint8_t RFM69::getNodeId()
+{
+  return m_node_id;
+}
+
+uint8_t RFM69::getBroadcastId()
+{
+  return 255;
+}
+
+void RFM69::setHighPower(bool high_power)
+{
+  m_high_power = high_power;
+}
+
+bool RFM69::isHighPower()
+{
+  return m_high_power;
+}
+
+void RFM69::setReceiveHandler(RFM69_receive_handler handler)
+{
+  m_receive_handler = handler;
+}
+
+void RFM69::sendMessage(uint8_t recipient, uint8_t * buffer, uint8_t len)
+{
+  m_seq_num = m_seq_num == 255 ? 1 : m_seq_num + 1;
+  m_send_seq_num = m_seq_num;
+  m_send_retries = 50;
+  m_recipient = recipient;
+  m_packet_length = len > RFM69_MAX_PACKET_LENGTH ? RFM69_MAX_PACKET_LENGTH : len;
+
+  for (uint8_t i = 0; i < m_packet_length; i++) {
+    m_packet_buffer[i] = buffer[i];
+  }
+
+  setPayloadReadyDio0Iterrupt();
+  setMode(RFM69_MODE_RX);
+  
+  DEBUGPRINTLN1("Waiting for channel to be free");
+
+  m_state = SEND_LBT;
+}
+
+void RFM69::transmitAck()
+{ 
+  select();
+  sendQueueWriteCommand();
+  
+  SPI.transfer(4);
+  SPI.transfer(m_ack_recipient);
+  SPI.transfer(getNodeId());
+  SPI.transfer(0);
+  SPI.transfer(m_ack_seq_num);
+  
+  unselect();
+}
+
+void RFM69::transmitMessage()
+{ 
+  select();
+  sendQueueWriteCommand();
+  
+  SPI.transfer(m_packet_length + 4);
+  SPI.transfer(m_recipient);
+  SPI.transfer(getNodeId());
+  SPI.transfer(m_send_seq_num);
+  SPI.transfer(0);
+
+  for (uint8_t i = 0; i < m_packet_length; i++) {
+    SPI.transfer(((uint8_t*) m_packet_buffer)[i]);
+  }
+  
+  unselect();
+}
+
+bool RFM69::isChannelFree()
+{
+  if (isPayloadReady()) {
+    writeReg(RFM69_REG_PACKET_CONFIG_2, readReg(RFM69_REG_PACKET_CONFIG_2) | 0x04);
+  }
+    
+  updateRssi();
+    
+  return getRssi() <= m_rssi_threshold;
+}
+
+void RFM69::beginTransmission()
+{
+  setMode(RFM69_MODE_STBY);
+  
+  if (isHighPower()) {
+    enableHighPower();
+  }
+      
+  setMode(RFM69_MODE_TX);
+  setPacketSentDio0Iterrupt();
+
+  m_tx_success = false;
+  m_tx_start = millis();
+}
+
+bool RFM69::isTransmissionFinished()
+{
+  return ((m_tx_success = (digitalRead(m_dio0_pin) == 1)) || millis() - m_tx_start >= 100);
+}
+
+void RFM69::endTransmission()
+{
+  setMode(RFM69_MODE_STBY);
+  disableHighPower();
+  setPayloadReadyDio0Iterrupt();
+
+  if (m_tx_success) {
+    DEBUGPRINTLN1("Transmission successful");
+  }
+}
+
+void RFM69::doReceiveCallback()
+{
+  if (m_receive_handler != NULL) {
+    (*m_receive_handler)(m_packet_buffer[2], m_recv_rssi, m_packet_buffer + 5, m_packet_length - 5);
+  }
+}
+
+void RFM69::receivePacket(uint8_t * buffer, uint8_t * length, uint8_t maxLength, int8_t * rssi)
+{
+  updateRssi();
+  *rssi = getRssi();
+
+  select();
+  sendQueueReadCommand();
+  
+  buffer[0] = SPI.transfer(0);
+  *length = buffer[0] + 1;
+
+  for (uint8_t i = 1; i < buffer[0] + 1; i++) {
+    uint8_t data = SPI.transfer(0);
+
+    if (i < maxLength) {
+      buffer[i] = data;
+    }
+  }
+
+  unselect();
+}
+
+void RFM69::receive()
+{
+  m_default_mode = RFM69_RECEIVE;
+  restoreDefaultMode();
+}
+
+void RFM69::listen()
+{
+  m_default_mode = RFM69_LISTEN;
+  restoreDefaultMode();
+}
+
+void RFM69::restoreDefaultMode()
+{
+  if (m_default_mode == RFM69_STANDBY) {
+    DEBUGPRINTLN1("Standby");
+    
+    setMode(RFM69_MODE_STBY);
+  } else if (m_default_mode == RFM69_RECEIVE) {
+    DEBUGPRINTLN1("Receiving");
+
+    setPayloadReadyDio0Iterrupt();
+    setMode(RFM69_MODE_RX);
+
+    m_state = RECV;
+  } else if (m_default_mode == RFM69_LISTEN) {
+    DEBUGPRINTLN1("Listening");
+  
+    setMode(RFM69_MODE_STBY);
+    waitForModeReady();
+    setPayloadReadyDio0Iterrupt();
+    writeReg(RFM69_REG_MODE, RFM69_MODE_STBY_LISTEN);
+
+    m_state = RECV;
+  }
+}
+
+bool RFM69::isInHistory(uint8_t sender, uint8_t seq_num)
+{
+  for (uint8_t i = 0; i < m_history_length; i++) {
+    if (m_history[i].sender == sender && m_history[i].seq_num == seq_num) {
+      return millis() - m_history[i].received < 1000;
+    }
+  }
+  
+  return false;
+}
+
+void RFM69::putInHistory(uint8_t sender, uint8_t seq_num)
+{
+  uint8_t position = m_history_end;
+
+  for (uint8_t i = 0; i < m_history_length; i++) {
+    if (m_history[i].sender == sender) {
+      position = i;
+      break;
+    }
+  }
+  
+  m_history[position].sender = sender;
+  m_history[position].seq_num = seq_num;
+  m_history[position].received = millis();
+  m_history_length = position == m_history_end && m_history_length < RFM69_HISTORY_SIZE ? m_history_length + 1 : m_history_length;
+  m_history_end = (m_history_end + 1) % RFM69_HISTORY_SIZE;
+}
+
+void RFM69::setChannel(uint8_t channel)
+{
+  if (!m_ready) {
+    return;
+  }
+  
+  setFrequency(m_frequency + (m_channel * m_channel_step));
+}
+
+void RFM69::calibrateOsc()
+{ 
+  writeReg(RFM69_REG_OSC_1, 0x00);
+  while (readReg(RFM69_REG_OSC_1) & 0x40 != 0x40);
+}
+
+void RFM69::setMode(uint8_t mode)
+{
+  writeReg(RFM69_REG_MODE, mode);
+}
+
+uint8_t RFM69::getMode()
+{
+  return readReg(RFM69_REG_MODE);
+}
+
+void RFM69::reset()
+{ 
+  digitalWrite(m_rst_pin, HIGH);
+  delayMicroseconds(200);
+  digitalWrite(m_rst_pin, LOW);
+  delay(10);
+}
+
+int8_t RFM69::getRssi()
+{
+  return -(m_rssi / 2);
+}
+
+void RFM69::setFrequency(uint32_t frequency)
+{
+  uint32_t frf = frequency / RFM69_FSTEP;
+  
+  writeReg(RFM69_REG_FRF_MSB, (uint8_t) (frf >> 16));
+  writeReg(RFM69_REG_FRF_MID, (uint8_t) (frf >> 8));
+  writeReg(RFM69_REG_FRF_LSB, (uint8_t) (frf));
+}
+
+void RFM69::setPacketSentDio0Iterrupt()
+{
+  writeReg(RFM69_REG_DIO_MAPPING_1, 0x00);
+}
+
+void RFM69::setPayloadReadyDio0Iterrupt()
+{
+  writeReg(RFM69_REG_DIO_MAPPING_1, 0x40);
+}
+
+void RFM69::setRssiDio0Iterrupt()
+{
+  writeReg(RFM69_REG_DIO_MAPPING_1, 0xC0);
+}
+
+bool RFM69::isModeReady()
+{
+  return (bool) (readReg(RFM69_REG_IRQ_FLAGS_1) & 0x80);
+}
+
+bool RFM69::isPayloadReady()
+{
+  //return (bool) (readReg(RFM69_REG_IRQ_FLAGS_2) & 0x04);
+
+  return (bool) digitalRead(m_dio0_pin);
+}
+
+void RFM69::sendQueueWriteCommand()
+{
+  SPI.transfer(0x00 | 0x80);
+}
+
+void RFM69::sendQueueReadCommand()
+{
+  SPI.transfer(0x00);
+}
+
+void RFM69::enableHighPower()
+{
+  writeReg(RFM69_REG_PA_LEVEL, 0x7F);
+  writeReg(RFM69_REG_OCP, 0x0F);
+  writeReg(RFM69_REG_TEST_PA_1, 0x5D);
+  writeReg(RFM69_REG_TEST_PA_2, 0x7C);
+}
+
+void RFM69::disableHighPower()
+{
+  writeReg(RFM69_REG_PA_LEVEL, 0x9F);
+  writeReg(RFM69_REG_OCP, 0x1A);
+  writeReg(RFM69_REG_TEST_PA_1, 0x55);
+  writeReg(RFM69_REG_TEST_PA_2, 0x70);
+}
+
+void RFM69::waitForModeReady()
+{
+  while (!isModeReady()) {
+    yield();
+  }
+}
+
+uint8_t RFM69::readReg(uint8_t addr)
+{
+  select();
+  SPI.transfer(addr & 0x7F);
+  uint8_t regval = SPI.transfer(0);
+  unselect();
+  
+  return regval;
+}
+
+void RFM69::writeReg(uint8_t addr, uint8_t value)
+{
+  select();
+  SPI.transfer(addr | 0x80);
+  SPI.transfer(value);
+  unselect();
+}
+
+void RFM69::select()
+{
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
+  
+  #ifdef __AVR__
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  #else
+  SPI.setClockDivider(SPI_CLOCK_DIV16);
+  #endif
+  
+  digitalWrite(m_ss_pin, LOW);
+}
+
+void RFM69::unselect()
+{
+  digitalWrite(m_ss_pin, HIGH);
+}
---
+++ RFM69.h
@@ -0,0 +1,133 @@
+#ifndef RFM69_H
+#define RFM69_H
+
+#ifdef __AVR__
+#include <Arduino.h>
+#else
+#include <c_types.h>
+#endif
+
+#include "RFM69_registers.h"
+
+#ifndef RFM69_MAX_PACKET_LENGTH
+#define RFM69_MAX_PACKET_LENGTH 60
+#endif // RFM69_MAX_PACKET_LENGTH
+
+#define RFM69_HISTORY_SIZE 3
+#define DEBUGLEVEL 1
+
+typedef void (*RFM69_receive_handler)(uint8_t sender, int8_t rssi, uint8_t * packet, uint8_t len);
+enum RFM69_STATE { STBY, RECV, ACK_WAIT, SEND_LBT, SEND_WAIT, SEND_WAIT_ACK };
+enum RFM69_MODE { RFM69_STANDBY, RFM69_LISTEN, RFM69_RECEIVE };
+
+struct RFM69ReceiveHistory {
+  uint8_t sender;
+  uint8_t seq_num;
+  unsigned long received;
+};
+
+class RFM69
+{
+  private:
+  
+  uint16_t m_long_preamble_millis = 1005;
+  uint32_t m_frequency = 867500032; // Hz
+  uint16_t m_channel_step = 50020; // Hz
+
+  uint8_t m_rst_pin;
+  uint8_t m_ss_pin;
+  uint8_t m_dio0_pin;
+  RFM69_STATE m_state = STBY;
+  RFM69_MODE m_default_mode = RFM69_STANDBY;
+  
+  uint8_t m_recipient;
+  uint8_t m_send_seq_num;
+  uint8_t m_send_retries;
+  uint8_t m_ack_recipient;
+  uint8_t m_ack_seq_num;
+
+  uint8_t m_tx_success = false;
+  unsigned long m_tx_start = 0;
+  unsigned long m_ack_wait_start = 0;
+
+  uint8_t m_ack_buffer[5];
+  uint8_t m_packet_buffer[RFM69_MAX_PACKET_LENGTH];
+  uint8_t m_packet_length = 0;
+
+  struct RFM69ReceiveHistory m_history[RFM69_HISTORY_SIZE];
+  uint8_t m_history_length = 0;
+  uint8_t m_history_end = 0;
+
+  int8_t m_rssi_threshold = -105; // dBm
+  int8_t m_recv_rssi;
+  uint8_t m_rssi;
+
+  uint8_t m_network_id = 100;
+  uint8_t m_node_id = 0;
+  uint8_t m_channel = 0;
+
+  bool m_high_power = false;
+  bool m_ready = false;
+  uint8_t m_seq_num = 0;
+
+  RFM69_receive_handler m_receive_handler = NULL;
+
+  public:
+
+  RFM69(uint8_t dio0_pin, uint8_t ss_pin, uint8_t rst_pin);
+  
+  void begin();
+  void update();
+  
+  void setNetworkId(uint8_t network_id);
+  uint8_t getNetworkId();
+  void setNodeId(uint8_t node_id);
+  uint8_t getNodeId();
+  uint8_t getBroadcastId();
+  void setChannel(uint8_t channel);
+  void setHighPower(bool high_power = true);
+  bool isHighPower();
+  void setReceiveHandler(RFM69_receive_handler handler);
+  void sendMessage(uint8_t recipient, uint8_t * buffer, uint8_t len);
+  bool isSending();
+  void waitForSend();
+  void receive();
+  void listen();
+  
+  private:
+  
+  void reset();
+  void setFrequency(uint32_t frequency);
+  void calibrateOsc();
+  void setMode(uint8_t mode);
+  uint8_t getMode();
+  void writeReg(uint8_t addr, uint8_t val);
+  uint8_t readReg(uint8_t addr);
+  void select();
+  void unselect();
+  void setPacketSentDio0Iterrupt();
+  void setPayloadReadyDio0Iterrupt();
+  void setRssiDio0Iterrupt();
+  void sendQueueWriteCommand();
+  void sendQueueReadCommand();
+  void enableHighPower();
+  void disableHighPower();
+  bool isModeReady();
+  bool isPayloadReady();
+  void waitForModeReady();
+  void updateRssi();
+  int8_t getRssi();
+  void transmitMessage();
+  void transmitAck();
+  bool isChannelFree();
+  void beginTransmission();
+  bool isTransmissionFinished();
+  void endTransmission();
+  void receivePacket(uint8_t * buffer, uint8_t * length, uint8_t maxLength, int8_t * rssi);
+  void doReceiveCallback();
+  bool isInHistory(uint8_t sender, uint8_t seq_num);
+  void putInHistory(uint8_t sender, uint8_t seq_num);
+  void restoreDefaultMode();
+};
+
+#endif // RFM69_H
